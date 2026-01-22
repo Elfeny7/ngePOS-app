c464059a6d9bbc5a346ba87eb79cfbe4222c131c
elfeny7 <ikmalfaris50@gmail.com>
2026-01-22 13:01:40 +0700

    feat: add tabs and icon

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function TabLayout() {
+    return (
+        <Tabs screenOptions={{ headerTitle: 'ngePOS' }} >
+            <Tabs.Screen name="index" options={{
+                title: 'Dashboard', tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="grid-outline" size={size} color={color} />
+                ),
+            }} />
+            <Tabs.Screen name="transaction" options={{
+                title: 'Transaction', tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="cash-outline" size={size} color={color} />
+                ),
+            }} />
+            <Tabs.Screen name="cart" options={{
+                title: 'Cart', tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="cart-outline" size={size} color={color} />
+                ),
+            }} />
+            <Tabs.Screen name="history" options={{
+                title: 'History', tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="time-outline" size={size} color={color} />
+                ),
+            }} />
+        </Tabs>
+    );
+}
